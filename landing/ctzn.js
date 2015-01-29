d5fd05ffb91631dda362b9ab5636f131afa6ab7e
Jim Thoburn <jim@jimthoburn.com>
2015-01-29 13:54:01 -0800

    Attempt to get field labels working in Opera Mini.

--- landing/ctzn.js
+++ landing/ctzn.js
@@ -3,9 +3,9 @@
 
   // OPTIONAL: Hide redundant labels in browsers that support the placeholder attribute.
   // http://stackoverflow.com/questions/8263891/simple-way-to-check-if-placeholder-is-supported
+  // http://stackoverflow.com/questions/22780534/placeholder-attribute-detected-in-opera-mini-but-not-used
   function supportsPlaceholder() {
-    var test = document.createElement("input");
-    return ("placeholder" in test);
+    return ('placeholder' in document.createElement('input') && 'placeholder' in document.createElement('textarea'));
   }
   if (supportsPlaceholder()) {
     var html = document.getElementsByTagName("html")[0];
