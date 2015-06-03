eef1774352a4f63199d9c42fad384af15abf2c25
Jim Thoburn <jim@jimthoburn.com>
2015-06-03 15:09:00 -0700

    Update example pages for getctzn.com

--- assets/js/supports.js
+++ assets/js/supports.js
@@ -1,15 +1,8 @@
 
 (function() {
-
-  // OPTIONAL: Hide redundant labels in browsers that support the placeholder attribute.
+  // OPTIONAL: Add an extra class for styling, in browsers that support the placeholder attribute.
   // http://stackoverflow.com/questions/8263891/simple-way-to-check-if-placeholder-is-supported
-  // http://stackoverflow.com/questions/22780534/placeholder-attribute-detected-in-opera-mini-but-not-used
-  function supportsPlaceholder() {
-    return ('placeholder' in document.createElement('input') && 'placeholder' in document.createElement('textarea'));
+  if ('placeholder' in document.createElement('input') && 'placeholder' in document.createElement('textarea')) {
+    var html = document.getElementsByTagName("html")[0]; html.className += " supports-placeholder";
   }
-  if (supportsPlaceholder()) {
-    var html = document.getElementsByTagName("html")[0];
-    html.className += " supports-placeholder";
-  }
-
 })();
