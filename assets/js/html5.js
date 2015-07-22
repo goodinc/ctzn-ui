9556c278258bd425c7fe0a504deceff41449f573
Jim Thoburn <jim@jimthoburn.com>
2015-07-21 20:27:05 -0700

    Update index

--- assets/js/html5.js
+++ assets/js/html5.js
@@ -1,5 +1,5 @@
 (function() {
-  // Attempt to make new HTML elements styleable in older browsers
+  // Make new HTML elements styleable in older browsers
   var elements = ['main', 'section', 'article', 'aside', 'nav', 'header', 'footer'];
   for (var index = 0; index < elements.length; index++) { document.createElement(elements[index]); }
 })();
