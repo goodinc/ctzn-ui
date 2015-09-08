b6e05b778b3eb27f48552115d542873898704359
Jim Thoburn <jim@jimthoburn.com>
2015-09-08 12:04:11 -0700

    Add support for “picture” in older browsers

--- assets/js/html5.js
+++ assets/js/html5.js
@@ -1,5 +1,5 @@
 (function() {
   // Make new HTML elements styleable in older browsers
-  var elements = ['main', 'section', 'article', 'aside', 'nav', 'header', 'footer', 'figure'];
+  var elements = ['main', 'section', 'article', 'aside', 'nav', 'header', 'footer', 'figure', 'picture'];
   for (var index = 0; index < elements.length; index++) { document.createElement(elements[index]); }
 })();
