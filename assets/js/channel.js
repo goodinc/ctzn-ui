e0c44f8c77c8273546a55e14fa7b5c00945943f2
Jim Thoburn <jim@jimthoburn.com>
2015-09-29 18:37:13 -0700

    Add latest styles for the Live Channel.

--- assets/js/channel.js
+++ assets/js/channel.js
@@ -1,4 +1,18 @@
 
+// This is the JavaScript entry point for the "live channel" page (the actual defined live channels and the individual push channels).
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// OPTIONAL: Make images look nice in browsers that don’t yet support the <picture> element.
+
+// require ('bower_components/picturefill/dist/picturefill');
+
+/*! Picturefill - v2.3.1 - 2015-04-09
+* http://scottjehl.github.io/picturefill
+* Copyright (c) 2015 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT */
+window.matchMedia||(window.matchMedia=function(){"use strict";var a=window.styleMedia||window.media;if(!a){var b=document.createElement("style"),c=document.getElementsByTagName("script")[0],d=null;b.type="text/css",b.id="matchmediajs-test",c.parentNode.insertBefore(b,c),d="getComputedStyle"in window&&window.getComputedStyle(b,null)||b.currentStyle,a={matchMedium:function(a){var c="@media "+a+"{ #matchmediajs-test { width: 1px; } }";return b.styleSheet?b.styleSheet.cssText=c:b.textContent=c,"1px"===d.width}}}return function(b){return{matches:a.matchMedium(b||"all"),media:b||"all"}}}()),function(a,b,c){"use strict";function d(b){"object"==typeof module&&"object"==typeof module.exports?module.exports=b:"function"==typeof define&&define.amd&&define("picturefill",function(){return b}),"object"==typeof a&&(a.picturefill=b)}function e(a){var b,c,d,e,f,i=a||{};b=i.elements||g.getAllElements();for(var j=0,k=b.length;k>j;j++)if(c=b[j],d=c.parentNode,e=void 0,f=void 0,"IMG"===c.nodeName.toUpperCase()&&(c[g.ns]||(c[g.ns]={}),i.reevaluate||!c[g.ns].evaluated)){if(d&&"PICTURE"===d.nodeName.toUpperCase()){if(g.removeVideoShim(d),e=g.getMatch(c,d),e===!1)continue}else e=void 0;(d&&"PICTURE"===d.nodeName.toUpperCase()||!g.sizesSupported&&c.srcset&&h.test(c.srcset))&&g.dodgeSrcset(c),e?(f=g.processSourceSet(e),g.applyBestCandidate(f,c)):(f=g.processSourceSet(c),(void 0===c.srcset||c[g.ns].srcset)&&g.applyBestCandidate(f,c)),c[g.ns].evaluated=!0}}function f(){function c(){clearTimeout(d),d=setTimeout(h,60)}g.initTypeDetects(),e();var d,f=setInterval(function(){return e(),/^loaded|^i|^c/.test(b.readyState)?void clearInterval(f):void 0},250),h=function(){e({reevaluate:!0})};a.addEventListener?a.addEventListener("resize",c,!1):a.attachEvent&&a.attachEvent("onresize",c)}if(a.HTMLPictureElement)return void d(function(){});b.createElement("picture");var g=a.picturefill||{},h=/\s+\+?\d+(e\d+)?w/;g.ns="picturefill",function(){g.srcsetSupported="srcset"in c,g.sizesSupported="sizes"in c,g.curSrcSupported="currentSrc"in c}(),g.trim=function(a){return a.trim?a.trim():a.replace(/^\s+|\s+$/g,"")},g.makeUrl=function(){var a=b.createElement("a");return function(b){return a.href=b,a.href}}(),g.restrictsMixedContent=function(){return"https:"===a.location.protocol},g.matchesMedia=function(b){return a.matchMedia&&a.matchMedia(b).matches},g.getDpr=function(){return a.devicePixelRatio||1},g.getWidthFromLength=function(a){var c;if(!a||a.indexOf("%")>-1!=!1||!(parseFloat(a)>0||a.indexOf("calc(")>-1))return!1;a=a.replace("vw","%"),g.lengthEl||(g.lengthEl=b.createElement("div"),g.lengthEl.style.cssText="border:0;display:block;font-size:1em;left:0;margin:0;padding:0;position:absolute;visibility:hidden",g.lengthEl.className="helper-from-picturefill-js"),g.lengthEl.style.width="0px";try{g.lengthEl.style.width=a}catch(d){}return b.body.appendChild(g.lengthEl),c=g.lengthEl.offsetWidth,0>=c&&(c=!1),b.body.removeChild(g.lengthEl),c},g.detectTypeSupport=function(b,c){var d=new a.Image;return d.onerror=function(){g.types[b]=!1,e()},d.onload=function(){g.types[b]=1===d.width,e()},d.src=c,"pending"},g.types=g.types||{},g.initTypeDetects=function(){g.types["image/jpeg"]=!0,g.types["image/gif"]=!0,g.types["image/png"]=!0,g.types["image/svg+xml"]=b.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1"),g.types["image/webp"]=g.detectTypeSupport("image/webp","data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=")},g.verifyTypeSupport=function(a){var b=a.getAttribute("type");if(null===b||""===b)return!0;var c=g.types[b];return"string"==typeof c&&"pending"!==c?(g.types[b]=g.detectTypeSupport(b,c),"pending"):"function"==typeof c?(c(),"pending"):c},g.parseSize=function(a){var b=/(\([^)]+\))?\s*(.+)/g.exec(a);return{media:b&&b[1],length:b&&b[2]}},g.findWidthFromSourceSize=function(c){for(var d,e=g.trim(c).split(/\s*,\s*/),f=0,h=e.length;h>f;f++){var i=e[f],j=g.parseSize(i),k=j.length,l=j.media;if(k&&(!l||g.matchesMedia(l))&&(d=g.getWidthFromLength(k)))break}return d||Math.max(a.innerWidth||0,b.documentElement.clientWidth)},g.parseSrcset=function(a){for(var b=[];""!==a;){a=a.replace(/^\s+/g,"");var c,d=a.search(/\s/g),e=null;if(-1!==d){c=a.slice(0,d);var f=c.slice(-1);if((","===f||""===c)&&(c=c.replace(/,+$/,""),e=""),a=a.slice(d+1),null===e){var g=a.indexOf(",");-1!==g?(e=a.slice(0,g),a=a.slice(g+1)):(e=a,a="")}}else c=a,a="";(c||e)&&b.push({url:c,descriptor:e})}return b},g.parseDescriptor=function(a,b){var c,d=b||"100vw",e=a&&a.replace(/(^\s+|\s+$)/g,""),f=g.findWidthFromSourceSize(d);if(e)for(var h=e.split(" "),i=h.length-1;i>=0;i--){var j=h[i],k=j&&j.slice(j.length-1);if("h"!==k&&"w"!==k||g.sizesSupported){if("x"===k){var l=j&&parseFloat(j,10);c=l&&!isNaN(l)?l:1}}else c=parseFloat(parseInt(j,10)/f)}return c||1},g.getCandidatesFromSourceSet=function(a,b){for(var c=g.parseSrcset(a),d=[],e=0,f=c.length;f>e;e++){var h=c[e];d.push({url:h.url,resolution:g.parseDescriptor(h.descriptor,b)})}return d},g.dodgeSrcset=function(a){a.srcset&&(a[g.ns].srcset=a.srcset,a.srcset="",a.setAttribute("data-pfsrcset",a[g.ns].srcset))},g.processSourceSet=function(a){var b=a.getAttribute("srcset"),c=a.getAttribute("sizes"),d=[];return"IMG"===a.nodeName.toUpperCase()&&a[g.ns]&&a[g.ns].srcset&&(b=a[g.ns].srcset),b&&(d=g.getCandidatesFromSourceSet(b,c)),d},g.backfaceVisibilityFix=function(a){var b=a.style||{},c="webkitBackfaceVisibility"in b,d=b.zoom;c&&(b.zoom=".999",c=a.offsetWidth,b.zoom=d)},g.setIntrinsicSize=function(){var c={},d=function(a,b,c){b&&a.setAttribute("width",parseInt(b/c,10))};return function(e,f){var h;e[g.ns]&&!a.pfStopIntrinsicSize&&(void 0===e[g.ns].dims&&(e[g.ns].dims=e.getAttribute("width")||e.getAttribute("height")),e[g.ns].dims||(f.url in c?d(e,c[f.url],f.resolution):(h=b.createElement("img"),h.onload=function(){if(c[f.url]=h.width,!c[f.url])try{b.body.appendChild(h),c[f.url]=h.width||h.offsetWidth,b.body.removeChild(h)}catch(a){}e.src===f.url&&d(e,c[f.url],f.resolution),e=null,h.onload=null,h=null},h.src=f.url)))}}(),g.applyBestCandidate=function(a,b){var c,d,e;a.sort(g.ascendingSort),d=a.length,e=a[d-1];for(var f=0;d>f;f++)if(c=a[f],c.resolution>=g.getDpr()){e=c;break}e&&(e.url=g.makeUrl(e.url),b.src!==e.url&&(g.restrictsMixedContent()&&"http:"===e.url.substr(0,"http:".length).toLowerCase()?void 0!==window.console&&console.warn("Blocked mixed content image "+e.url):(b.src=e.url,g.curSrcSupported||(b.currentSrc=b.src),g.backfaceVisibilityFix(b))),g.setIntrinsicSize(b,e))},g.ascendingSort=function(a,b){return a.resolution-b.resolution},g.removeVideoShim=function(a){var b=a.getElementsByTagName("video");if(b.length){for(var c=b[0],d=c.getElementsByTagName("source");d.length;)a.insertBefore(d[0],c);c.parentNode.removeChild(c)}},g.getAllElements=function(){for(var a=[],c=b.getElementsByTagName("img"),d=0,e=c.length;e>d;d++){var f=c[d];("PICTURE"===f.parentNode.nodeName.toUpperCase()||null!==f.getAttribute("srcset")||f[g.ns]&&null!==f[g.ns].srcset)&&a.push(f)}return a},g.getMatch=function(a,b){for(var c,d=b.childNodes,e=0,f=d.length;f>e;e++){var h=d[e];if(1===h.nodeType){if(h===a)return c;if("SOURCE"===h.nodeName.toUpperCase()){null!==h.getAttribute("src")&&void 0!==typeof console&&console.warn("The `src` attribute is invalid on `picture` `source` element; instead, use `srcset`.");var i=h.getAttribute("media");if(h.getAttribute("srcset")&&(!i||g.matchesMedia(i))){var j=g.verifyTypeSupport(h);if(j===!0){c=h;break}if("pending"===j)return!1}}}}return c},f(),e._=g,d(e)}(window,window.document,new window.Image);
+
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // OPTIONAL: Test layout before using animation (see note below about flexible box layout)
 
@@ -11,7 +25,7 @@
 // Respond to a few special keys on the keyboard
 
 (function() {
-  if (document.querySelector && document.addEventListener) {
+    if (!document.querySelector || !document.addEventListener) return;
 
     var SPACE_KEY = 32;
     var LEFT_ARROW_KEY = 37;
@@ -39,7 +53,6 @@
 
     }, false);
 
-  }
 })();
 
 
@@ -134,12 +147,14 @@
 // Turn the page into an autoplaying slideshow
 
 (function() {
-  if (document.querySelector && document.addEventListener) {
+
+    if (!document.querySelector || !document.addEventListener) return;
 
     var textShowing;
     var currentItem;
     var currentImage;
     var lastItem;
+    var lastImage;
     var timer;
 
     function hasClassName(element, className) {
@@ -218,12 +233,12 @@
     function updatePage() {
       var items = document.querySelectorAll('main .list .item');
 
-      // Show the current item
       if (lastItem) addClassName(lastItem, 'inactive');
       removeClassName(currentItem, 'inactive');
 
       textShowing = false;
       currentImage = 0;
+      lastImage = undefined;
       next();
 
       if (lastItem) {
@@ -307,13 +322,6 @@
       }
     }
 
-    // KUDOS: http://stackoverflow.com/questions/10964966/detect-ie-version-prior-to-v9-in-javascript#answer-10965203
-    function isIE9() {
-      var div = document.createElement("div");
-      div.innerHTML = "<!--[if IE 9]><i></i><![endif]-->";
-      return div.getElementsByTagName("i").length === 1;
-    }
-
     function addItem(index) {
       if (document.getElementById('item-' + index)) return;
 
@@ -322,17 +330,31 @@
       var html = (window.dataItems && window.dataItems[index]) ? dataItems[index] : template ? template.innerHTML : null;
 
       if (html == null) {
-        if (window.updateItemsData) window.updateItemsData();
         return;
       }
 
       var item = document.createElement('div');
 
-      // OPTIONAL: Remove the src attribute of the <img> element, if running picturefill (to avoid a double download)
-      // TRICKY: IE9 needs a src or srcset on the <img>, due to a bug with <source> elements: http://scottjehl.github.io/picturefill/examples/demo-02.html
-      if (window.picturefill && !isIE9()) {
-        html = html.replace(/(<img class="figure")[\s]+src="[^"]*"/g, function(match, p1) { return p1; });
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      // OPTIONAL: Picturefill (part 1)
+      //
+      //           Move the “src” attribute of the <img> element to a data attribute, if running picturefill (to avoid a double download)
+      //           (Later, we’ll manually move it back, if picturefill has trouble choosing an image.)
+      //
+      // TRICKY:   Internet Explorer 9 needs a “src” or “srcset” on the <img>, due to a bug with <source> elements:
+      //           http://scottjehl.github.io/picturefill/examples/demo-02.html
+      //
+      // KUDOS: http://stackoverflow.com/questions/10964966/detect-ie-version-prior-to-v9-in-javascript#answer-10965203
+      function isIE9() {
+        var div = document.createElement("div");
+        div.innerHTML = "<!--[if IE 9]><i></i><![endif]-->";
+        return div.getElementsByTagName("i").length === 1;
       }
+
+      if (window.picturefill && !isIE9()) {
+        html = html.replace(/(<img class="figure")[\s]+src="([^"]*)"/g, function(match, p1, p2) { return p1 + 'data-channel-src="' + p2 + '"'; });
+      }
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
       item.className = 'item inactive';
       item.innerHTML = html;
@@ -346,8 +368,32 @@
         document.querySelector('.list').appendChild(item);
       }
 
-      // OPTIONAL: Run picturefill on the new images: https://scottjehl.github.io/picturefill/#api
-      if (window.picturefill) window.picturefill({ elements: item.getElementsByTagName('img') });
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      // OPTIONAL: Picturefill (part 2)
+      //
+      // (Highly recommended if running part 1)
+      //
+      if (window.picturefill) {
+        var images = item.querySelectorAll('img');
+
+        // Run picturefill on the new images: https://scottjehl.github.io/picturefill/#api
+        window.picturefill({ elements: images });
+
+        // Handle the case where picturefill doesn’t choose an image.
+        for (var index = 0; index < images.length; index++) {
+          var image = images[index];
+          if (!image.getAttribute('src') || image.getAttribute('src') === '') {
+            var originalSource = image.getAttribute('data-channel-src');
+            if (originalSource) {
+              image.setAttribute('src', originalSource);
+            } else {
+              if (console && console.error) console.error('Something went wrong while using picturefill. The “src” attribute of an image is missing.');
+            }
+          }
+        }
+      }
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     }
 
     function getItemNumber(item) {
@@ -505,18 +551,32 @@
       return duration;
     }
 
+    // Return “true” if the image data has started arriving.
     var testImage = new Image();
     var naturalWidthSupported = ('naturalWidth' in testImage);
     function imageLoaded(figure) {
       var image = figure.querySelector('img');
       if (naturalWidthSupported) {
-        if (image.naturalWidth > 0) {
+
+        // If the image has dimensions
+        if (image.naturalWidth > 0 && image.naturalHeight > 0) {
+
+          // Calculate the aspect ratio
           if (window.onChannelImageLoad) window.onChannelImageLoad(image);
+
+          // And assume it has loaded
           return true;
-        } else {
+
+        // If the image didn’t fail to load
+        } else if (!figure.getAttribute('data-no-dimensions')) {
+
+          // Assume it’s still loading
           return false;
         }
       }
+
+      // Return nothing if we don’t know.
+      return;
     }
 
     function next() {
@@ -553,15 +613,16 @@
       // If there are images to show
       if (images.length > 0) {
 
-        // Hide all of the images, except the current one
-        // TBD: Make this more efficient, by only doing it on the first loop?
-        for (var index = 0; index < images.length; index++) {
-          if (images[index] !== images[currentImage]) {
-            addClassName(images[index], 'inactive');
+        // Hide all of the images
+        if (!lastImage) {
+          for (var index = 0; index < images.length; index++) {
+            if (index !== currentImage) {
+              addClassName(images[index], 'inactive');
+            }
           }
         }
 
-        // Show the current image
+        if (lastImage && lastImage !== currentImage) addClassName(images[lastImage], 'inactive');
         removeClassName(images[currentImage], 'inactive');
 
         var animationDuration = duration;
@@ -595,6 +656,7 @@
             if (!textShowing) {
 
               // Display the next image
+              lastImage = currentImage;
               currentImage++;
             }
           }
@@ -602,7 +664,6 @@
       }
 
       // If the current image hasn’t finished loading
-      /*
       if (images[currentImage] && imageLoaded(images[currentImage]) === false) {
         nextStep = next;
         addClassName(document.body, 'loading');
@@ -610,12 +671,11 @@
       } else if (hasClassName(document.body, 'loading')) {
         removeClassName(document.body, 'loading');
 
-        // If the current image just finished loading, show it for three seconds
+        // If the current image just finished loading, show it for double the usual duration.
         if (images[currentImage] && imageLoaded(images[currentImage]) === true) {
-          duration = 3;
+          duration = 6;
         }
       }
-      */
 
       if (!paused) {
         timer = setTimeout(nextStep, duration * 1000);
@@ -623,24 +683,6 @@
            addClassName(document.body, 'autoplaying');
       }
     }
-
-    document.querySelector('nav').addEventListener('click', function(e) {
-
-      // If the user is pressing a modifier key, let the browser handle it
-      if (e && (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey)) return;
-
-      var item = closest(e.target, 'li');
-      if (item) {
-        if (timer) clearTimeout(timer);
-        if (item.className === 'next') {
-          nextPage();
-        } else {
-          previousPage();
-        }
-
-        e.preventDefault();
-      }
-    }, false);
 
     var paused;
     function startPlaying() {
@@ -685,6 +727,7 @@
       }, false);
     })();
 
+
     (function() {
 
       // Start with the first item
@@ -701,8 +744,26 @@
       }
     })();
 
+    document.querySelector('nav').addEventListener('click', function(e) {
+
+      // If the user is pressing a modifier key, let the browser handle it
+      if (e && (e.shiftKey || e.ctrlKey || e.altKey || e.metaKey)) return;
+
+      var item = closest(e.target, 'li');
+      if (item) {
+        if (timer) clearTimeout(timer);
+        if (item.className === 'next') {
+          nextPage();
+        } else {
+          previousPage();
+        }
+
+        e.preventDefault();
+      }
+    }, false);
+
     startPlaying();
-  }
+
 })();
 
 
@@ -710,6 +771,7 @@
 // Periodically retrieve updated list of posts
 
 (function() {
+
   var updateEvery = 5 * 60; // 5 minutes
 
   var inProgress = false;
@@ -719,7 +781,9 @@
     if (inProgress) return;
 
     var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function() {
+    function onResponse() {
+      if (xhr.readyState != 4) return;
+      inProgress = false;
 
       try {
         var count = 0;
@@ -746,20 +810,14 @@
         if (console && console.error) console.error('ERROR getting posts via XHR', e);
       }
 
-    });
-    xhr.addEventListener('loadend', function() {
-      inProgress = false;
-    });
-    var url = window.location.href + ((window.location.href.indexOf('?') >= 0) ? '&' : '?') + 'datatemplates=1';
+    };
+    var url = window.location.href;
+    xhr.onreadystatechange = onResponse;
     xhr.open('GET', url);
     xhr.send();
     inProgress = true;
   }
 
   setInterval(updateItemsData, updateEvery * 1000);
-  window.updateItemsData = updateItemsData;
-
-  // Fetch all of the posts right away, since the page only contains the first few items.
-  updateItemsData();
 })();
 
