8ab05e4c929ee6c67b91e742c3bb3700a0fe072e
Jim Thoburn <jim@jimthoburn.com>
2015-11-18 18:59:53 -0800

    Try a date picker on the new action form.

--- assets/js/placeholder.js
+++ assets/js/placeholder.js
@@ -1,8 +1,18 @@
 
 (function() {
-  // OPTIONAL: Add an extra class for styling, in browsers that support the placeholder attribute.
+  // OPTIONAL: Add an extra class for styling, in browsers that support the placeholder attribute on fields.
   // http://stackoverflow.com/questions/8263891/simple-way-to-check-if-placeholder-is-supported
   if ('placeholder' in document.createElement('input') && 'placeholder' in document.createElement('textarea')) {
     var html = document.getElementsByTagName("html")[0]; html.className += " supports-placeholder";
+  }
+})();
+
+(function() {
+  // OPTIONAL: Add an extra class for styling, in browsers that support the type="date" value on fields.
+  // http://stackoverflow.com/questions/18020950/how-to-make-input-type-date-supported-on-all-browsers-any-alternatives#answer-18021130
+  var test = document.createElement('input');
+  test.setAttribute('type', 'date');
+  if (test.type === 'date') {
+    var html = document.getElementsByTagName("html")[0]; html.className += " supports-date";
   }
 })();
