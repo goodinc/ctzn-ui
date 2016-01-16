178ee824e7902ef6a6f614db2c8d649e71eb8fdd
Jim Thoburn <jim@jimthoburn.com>
2016-01-15 16:12:52 -0800

    Tweak marker size

--- design/usdac-map-data.js
+++ design/usdac-map-data.js
@@ -7,7 +7,7 @@ var _ctzn_geoJson = [
     },
     "properties": {
       "marker-color": "#FDCE10",
-      "marker-size": "large",
+      "marker-size": "medium",
       "video": '<h6>State of the Union, DC</h6><p class="date">February 7, 2016</p><p class="location">1714 14th St NW, Washington DC</p><iframe width="200" height="200" src="http://www.ctznapp.com/push/93311448/dare-to-imagine?channel=1&q=user:brittanydelany&t=DareToImagine%20at%20On%20The%20Outside&page=2" frameborder="0" allowfullscreen></iframe>',
     }
   },
@@ -19,7 +19,7 @@ var _ctzn_geoJson = [
     },
     "properties": {
       "marker-color": "#FDCE10",
-      "marker-size": "large",
+      "marker-size": "medium",
       "video": '<h6>State of the Union, SF</h6><p class="date">February 2, 2016</p><p class="location">155 9th St, San Francisco</p><iframe width="200" height="200" src="http://www.ctznapp.com/push/93311448/dare-to-imagine?channel=1&q=user:joy&t=Music%20Flash%20Mob%20At%20Chase%20Plaza&page=2" frameborder="0" allowfullscreen></iframe>'
     }
   }
